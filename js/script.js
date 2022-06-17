1fed95a19b7c61762709c7436950ff943e905764
Nik <example@gmail.com>
2022-06-17 19:56:35 +0300

    старт напис.прилож

--- js/script.js
+++ js/script.js
@@ -1,156 +1,49 @@
-// "use strict";
+/* Задание на урок:
 
-// a = 15;
-// console.log(a);
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-// let number = 5;
-// const leftBorderWidth = 1;
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// number = 10;
-// console.log(number);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-// const obj = {
-//     a: 50
-// };
+Проверить, чтобы все работало без ошибок в консоли */
 
-// obj.a = 10;
+// 1):
 
-// console.log(obj);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// {
-//     var result = 50;
-// }
+// 2):
 
-// console.log(result);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// alert(5);
-// [].push('a');
+// 3):
 
-// const vehicleBodyWidth = 5000,
-//       vehicleBodyLength = 4000;
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('Один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
 
-// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// ['sasha', 'masha', 'dasha'].map(a => )
+console.log(personalMovieDB);
 
-// Reguest
-// data
-// response
-
-// snake_case
-// UPPER_SNAKE_CASE
-// Kebab-case
-// PascalCase
-
-// let COLOR_RED = '#f00';
-
-// "use strict";
-
-// let number = 4.6;
-
-// console.log(-4/0);
-// console.log('string' * 9);
-
-// const persone = `5`;
-
-// const bool = false;
-
-// console.log(something);
-
-// let und;
-// console.log(und);
-
-// const obj = {
-//     name: "John",
-//     age: 25,
-//     isMarried: false
-// };
-
-// console.log(obj.isMarried);
-
-
-// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
-// console.log(arr[1]);
-
-// const arr = ['a', 'b', 'c'];
-
-// arr[10] = '3456';
-
-// console.log(arr);
-
-// const arrObj = {
-//     a: 'a',
-//     '1': 'b',
-//     2: 'c',
-//     abc: {
-//         df: [{}, {}],
-//         def:{
-
-//         }
-//     }
-// };
-// const b = 'b';
-
-// arrObj.b = '1234';
-// arrObj[b] = [];
-
-// console.log(arrObj['b']);
-// console.log(arrObj.b);
-
-// // const obj = {a: 1, b: 2};
-// const obj = {
-//        Anna: 500,
-//        'Alice': 800
-// };
-
-// "use strict";
-
-// alert('Hello');
-
-// const result = confirm("Are you here?");
-// console.log(result);
-
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(answer + 5);
-
-// const answers = [];
-
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваше фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
-
-// console.log(typeof(answers));
-// console.log(typeof(null));
-
-// "use strict";
-
-// const category = 'toys';
-
-// console.log(`https://someurl.com/${category}/5`);
-
-// const user = "Nik";
-
-// alert(`привет, ${user}`);
-
-// "use strict";
-
-// console.log('arr' + " - object");
-// console.log(4  +"5");
-
-// let incr = 10,
-//     decr = 10;
-
-//     // incr++;
-//     // decr--;
-
-//     console.log(++incr);
-//     console.log(--decr);
-
-//     console.log(5%2);
-
-//     console.log(2 + 2 * 2 != '6');
-
-//     const isChecked = false,
-//           isClose = false;
-
-//           console.log(isChecked || !isClose);
