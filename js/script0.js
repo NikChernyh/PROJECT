5c7c456197491f06701b4f452fdfd00dcd4acb0c
Nik <example@gmail.com>
2022-06-20 17:39:03 +0300

    УСЛОВИЯ

--- js/script0.js
+++ js/script0.js
@@ -155,4 +155,40 @@
 
 //           console.log(isChecked || !isClose);
 
+// "use strict";
+
+// if (1) {
+//     console.log('ok!');
+// } else {
+// console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+// const num = '50';
+
+// switch (num) {
+//    case '49': 
+//      console.log('Неверно');
+//      break;
+//    case '100':
+//      console.log('Неверно');
+//      break;
+//    case '50':
+//     console.log('В точку!');
+//     break;
+//    default:
+//     console.log('Не в этот раз');
+//     break;
+// }
 "use strict";
