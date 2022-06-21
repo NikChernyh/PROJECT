563e1c516ab82fdf0d9bf835e139fee9b014c695
Nik <example@gmail.com>
2022-06-21 21:59:57 +0300

    Решение задачек!!!

--- js/script0.js
+++ js/script0.js
@@ -304,4 +304,42 @@
 //     }
 // }
 
+// "use strict";
+
+// Место для первой задачи
+// function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    // const arr = [3, 5, 8, 16, 20, 23, 50];
+    // const result = [];
+
+    // Пишем решение вот тут
+//     for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+    
+    // Не трогаем
+//     return result;
+// }
+
+// Место для второй задачи
+// function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    // const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
 "use strict";
