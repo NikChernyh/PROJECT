81f29a89fe702e988873f422da8ddc48e6da312b
Nik <example@gmail.com>
2022-06-22 15:16:57 +0300

    Start
    Coding Exercise 6: Задание на работу с функциями

--- js/script.js
+++ js/script.js
@@ -23,17 +23,17 @@
 
 // 1):
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 // 2):
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 // 3):
 
@@ -47,28 +47,28 @@ const personalMovieDB = {
 
 //   "use strict";
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
 
-          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-          personalMovieDB.movies[a] = b;
-          console.log('done');
-          } else {
-            console.log('error');
-            i--;
-          }
-}
+//           if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//           personalMovieDB.movies[a] = b;
+//           console.log('done');
+//           } else {
+//             console.log('error');
+//             i--;
+//           }
+// }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.coun < 30) {
-     console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.coun < 30) {
+//      console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка");
+// }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
--- js/script0.js
+++ js/script0.js
@@ -424,4 +424,33 @@
 // function doNothing() {};
 // console.log(doNothing() === undefined);
 
-"use strict";
+// "use strict";
+
+// Место для первой задачи
+// function sayHello(name) {
+//     return `Привет, ${name}!`;
+//     }
+//     sayHello('Alex');
+    
+    // Место для второй задачи
+    // function returnNeighboringNumbers(num) {
+    //     return [num - 1, num,num + 1];
+    // }
+    // returnNeighboringNumbers(5);
+    // Место для третьей задачи
+    // function getMathResult(num,times) {
+    //     if (typeof(times) !== 'number' || times <= 0) {
+    //         return num;
+    //     }
+    // let str = '';
+    // for (let i = 1; i <= times; i++) {
+    //     if (i === times) {
+    //         str += `${num * i}`;
+    //     } else {
+    //         str += `${num * i}---`;
+    //     }
+    // }
+    // return str;
+    // }
+
+    "use strict";
