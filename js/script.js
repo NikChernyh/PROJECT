c7c804603ad3792e3e52cd8d2dd20c19f94f7422
Nik <example@gmail.com>
2022-06-25 19:14:55 +0300

    метод trim()

--- js/script.js
+++ js/script.js
@@ -74,22 +74,22 @@ const personalMovieDB = {
 //           }
 // }
 
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//               b = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = prompt('На сколько оцените его?', '');
     
-//               if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//               personalMovieDB.movies[a] = b;
-//               console.log('done');
-//               } else {
-//                 console.log('error');
-//                 i--;
-//               }
-//     }
-// }
+              if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+              personalMovieDB.movies[a] = b;
+              console.log('done');
+              } else {
+                console.log('error');
+                i--;
+              }
+    }
+}
 
-// rememberMyFilms();
+rememberMyFilms();
 
 // if (personalMovieDB.count < 10) {
 //     console.log("Просмотрено довольно мало фильмов");
