c97f16b22b04d97dce77241cf9f1f5a935c391e7
Nik <example@gmail.com>
2022-06-29 13:54:58 +0300

    Основы ООП, прототипно-ориентированное наследование

--- js/script0.js
+++ js/script0.js
@@ -805,4 +805,35 @@
 
 // availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
 
+// "use strict";
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log("Hello");
+//     }
+// };
+
+// const jonh = Object.create(soldier); ***
+
+// const jonh = {
+//     health: 100
+// };
+
+// jonh.__proto__ = soldier;  устаревший код
+
+// Object.setPrototypeOf(jonh, soldier); ***
+
+// console.log(jonh.armor);
+// jonh.sayHello();
+
 "use strict";
