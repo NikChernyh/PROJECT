d250a6e3c4d382e88cb4138ade206aea6dc91b90
Nik <example@gmail.com>
2023-01-24 16:39:38 +0200

    48. События на мобильных устройствах

--- js/script0.js
+++ js/script0.js
@@ -1169,3 +1169,38 @@ function getTotalProgressByRecursion(data) {
 const result = getTotalProgressByRecursion(students);
 
 console.log(result[0]/result[1]);
+
+// События на мобильных устройствах
+
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('Start');
+        console.log(e.targetTouches);
+    });
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log('Move');
+    });
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+
+        console.log('End');
+    });
+});
+
+// touches
+// targetTouches
+// changedTouches
